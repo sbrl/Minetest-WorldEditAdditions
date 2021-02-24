191f1385b142c9ad6ed1e66443079a6ce282f40e
VorTechnix <45538536+VorTechnix@users.noreply.github.com>
2021-02-24 09:08:49 -0800

    fixed modulo

--- worldeditadditions/utils/selector_helps.lua
+++ worldeditadditions/utils/selector_helps.lua
@@ -1,6 +1,6 @@
 function worldeditadditions.player_axis2d(name)
   -- minetest.get_player_by_name("singleplayer"):
-	local dir = math.floor(minetest.get_player_by_name(name):get_look_horizontal() / math.pi * 2 + 0.5) % 3
+	local dir = math.floor(minetest.get_player_by_name(name):get_look_horizontal() / math.pi * 2 + 0.5) % 4
 	local crdnl = { {1,"z"},{-1,"x"},{-1,"z"},{1,"x"} }
   return crdnl[dir+1]
 end
