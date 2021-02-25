b62e4f706d1817ca5a2c767d88d921913cbbec3a
VorTechnix <45538536+VorTechnix@users.noreply.github.com>
2021-02-24 19:42:24 -0800

    added description

--- worldeditadditions/utils/selector_helps.lua
+++ worldeditadditions/utils/selector_helps.lua
@@ -1,3 +1,6 @@
+-- Returns the player's facing direction on the horizontal axes only.
+-- @param	name	string	The name of the player to return facing direction of.
+-- @return	table	Returns axis name and sign multiplyer.
 function worldeditadditions.player_axis2d(name)
   -- minetest.get_player_by_name("singleplayer"):
 	local dir = math.floor(minetest.get_player_by_name(name):get_look_horizontal() / math.pi * 2 + 0.5) % 4
