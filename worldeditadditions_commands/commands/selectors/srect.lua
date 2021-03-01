e1755ce8e13d2ce1679f0ef09d522452049fe39f
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2021-03-01 21:08:32 +0000

    srect: localify parsing function

--- worldeditadditions_commands/commands/selectors/srect.lua
+++ worldeditadditions_commands/commands/selectors/srect.lua
@@ -3,42 +3,37 @@
 -- ███████ ██████  █████   ██         ██
 --      ██ ██   ██ ██      ██         ██
 -- ███████ ██   ██ ███████  ██████    ██
--- local -- TODO: set this to local once development is finished
-function parse_params_srect(params_text)
-	local wea = worldeditadditions
-	local find = wea.split(params_text, "%s", false)
-	local ax1, ax2, len = find[1], find[2], find[table.maxn(find)]
-	
-	-- If ax1 is bad set to player facing dir
-	if ax1 == len  or not ax1:match('[xyz]') then ax1 = "get"
-	else
-		local success, value = wea.getsign(ax1, "int")
-		if not success then return success, value
-		else ax1 = { value, ax1:gsub('[^xyz]',''):sub(1,1) }
-		end
-	end
-	-- If ax2 is bad set to +y
-	if not ax2 or ax2 == len or not ax2:match('[xyz]') then ax2 = "y" end
-	local success, value = wea.getsign(ax2, "int")
-	if not success then return success, value end
-	ax2 = { value, ax2:gsub('[^xyz]',''):sub(1,1) }
-	
-	len = tonumber(len)
-	-- If len == nill cancel the operation
-	if len == nil then
-		return false, "No length specified."
-	end
-	
-	return true, ax1, ax2, len
-end
 worldedit.register_command("srect", {
 	params = "[<axis1> [<axis2>]] <length>",
 	description = "Set WorldEdit region position 2 at a set distance along 2 axes.",
 	privs = {worldedit=true},
 	require_pos = 1,
 	parse = function(params_text)
-		local values = {parse_params_srect(params_text)}
-		return unpack(values)
+		local wea = worldeditadditions
+		local find = wea.split(params_text, "%s", false)
+		local ax1, ax2, len = find[1], find[2], find[table.maxn(find)]
+		
+		-- If ax1 is bad set to player facing dir
+		if ax1 == len  or not ax1:match('[xyz]') then ax1 = "get"
+		else
+			local success, value = wea.getsign(ax1, "int")
+			if not success then return success, value
+			else ax1 = { value, ax1:gsub('[^xyz]',''):sub(1,1) }
+			end
+		end
+		-- If ax2 is bad set to +y
+		if not ax2 or ax2 == len or not ax2:match('[xyz]') then ax2 = "y" end
+		local success, value = wea.getsign(ax2, "int")
+		if not success then return success, value end
+		ax2 = { value, ax2:gsub('[^xyz]',''):sub(1,1) }
+		
+		len = tonumber(len)
+		-- If len == nill cancel the operation
+		if len == nil then
+			return false, "No length specified."
+		end
+		
+		return true, ax1, ax2, len
 	end,
 	func = function(name, axis1, axis2, len)
 		if axis1 == "get" then axis1 = worldeditadditions.player_axis2d(name) end
