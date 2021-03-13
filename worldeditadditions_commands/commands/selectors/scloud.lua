cc20297b889139d8812515070b573703d58f00fb
VorTechnix <45538536+VorTechnix@users.noreply.github.com>
2021-03-12 17:03:37 -0800

    scloud.lua typo fix
    Co-authored-by: Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>

--- worldeditadditions_commands/commands/selectors/scloud.lua
+++ worldeditadditions_commands/commands/selectors/scloud.lua
@@ -6,7 +6,7 @@
 local wea = worldeditadditions
 worldedit.register_command("scloud", {
 	params = "0-6/stop/reset",
-	description = "Set and add to WorldEdit region by punching put to six nodes that define the maximums of your target",
+	description = "Set and add to WorldEdit region by punching up to six nodes that define the maximums of your target",
 	privs = {worldedit=true},
 	parse = function(param)
 		return true, param
