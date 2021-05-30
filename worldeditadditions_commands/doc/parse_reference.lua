5083ea05ca25212e4b42e02668e74303bf4b8f34
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2021-05-30 16:18:08 +0100

    Start on #55, but because Minetest blocks our read attempts to Chat-Command-Reference (symlinks don't help), we can't continue this

---
+++ worldeditadditions_commands/doc/parse_reference.lua
@@ -0,0 +1,45 @@
+
+
+local function get_reference()
+	local lines = {}
+	for line in io.lines(worldeditadditions_commands.modpath.."/Chat-Command-Reference.md") do
+		table.insert(lines, line)
+	end
+	return lines
+end
+
+local function group_by_heading(lines, max_level)
+	local groups = {}
+	local acc = {}
+	
+	for i,line in ipairs(lines) do
+		if worldeditadditions.str_starts(line, "#") then
+			local _, _, heading, headingtext = line:find("(#+)%s*(.*)")
+			if #heading <= max_level then
+				table.insert(groups, {
+					level = #heading,
+					heading = headingtext,
+					text = table.concat(acc, "\n")
+				})
+				acc = {}
+			end
+		else
+			table.insert(acc, line)
+		end
+	end
+	return groups
+end
+
+function worldeditadditions.doc.parse_reference()
+	local lines = get_reference()
+	local headings = worldeditadditions.table_filter(
+		group_by_heading(lines, 2),
+		function(item, i) return item.level ~= 2 end
+	)
+	for i,value in ipairs(headings) do
+		print(i, "level", level, "heading", heading, "text", text)
+	end
+end
+
+
+worldeditadditions.doc.parse_reference()
