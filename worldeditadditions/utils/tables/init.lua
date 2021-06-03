0026bcdab7270d858883ea041077cbf36f6ffbf8
VorTechnix <45538536+VorTechnix@users.noreply.github.com>
2021-06-02 19:18:49 -0700

    added sets.lua, reordered dofiles in /tables/init.luas

--- worldeditadditions/utils/tables/init.lua
+++ worldeditadditions/utils/tables/init.lua
@@ -10,11 +10,12 @@
 
 -- TODO: Refactor into its own worldeditadditions.tables namespace.
 
+dofile(worldeditadditions.modpath.."/utils/tables/sets.lua")
 dofile(worldeditadditions.modpath.."/utils/tables/shallowcopy.lua")
 dofile(worldeditadditions.modpath.."/utils/tables/table_apply.lua")
-dofile(worldeditadditions.modpath.."/utils/tables/table_unpack.lua")
-dofile(worldeditadditions.modpath.."/utils/tables/table_get_last.lua")
-dofile(worldeditadditions.modpath.."/utils/tables/table_tostring.lua")
-dofile(worldeditadditions.modpath.."/utils/tables/table_map.lua")
 dofile(worldeditadditions.modpath.."/utils/tables/table_filter.lua")
+dofile(worldeditadditions.modpath.."/utils/tables/table_get_last.lua")
+dofile(worldeditadditions.modpath.."/utils/tables/table_map.lua")
+dofile(worldeditadditions.modpath.."/utils/tables/table_tostring.lua")
 dofile(worldeditadditions.modpath.."/utils/tables/table_unique.lua")
+dofile(worldeditadditions.modpath.."/utils/tables/table_unpack.lua")
