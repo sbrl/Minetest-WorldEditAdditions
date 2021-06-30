11bf486bf2cdbfa69a3a5a48a26076fd54b9053e
VorTechnix <45538536+VorTechnix@users.noreply.github.com>
2021-06-30 11:14:31 -0700

    Update init.lua

--- worldeditadditions_core/register/init.lua
+++ worldeditadditions_core/register/init.lua
@@ -6,8 +6,6 @@
 
 -- WorldEditAdditions Register/Overwrite Functions
 
---TODO: Replicate chatcommand_handler functionality (worldedit_commands/init.lua line 21-60)
-
 local we_cm = worldeditadditions_core.modpath .. "/register/"
 
 dofile(we_cm.."check.lua")
