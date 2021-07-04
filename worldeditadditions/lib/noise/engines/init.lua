8a14d35c0409f1a812358484522d4bfc2639e5af
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2021-07-04 16:14:48 +0100

    noise2d: add comment

--- worldeditadditions/lib/noise/engines/init.lua
+++ worldeditadditions/lib/noise/engines/init.lua
@@ -4,4 +4,6 @@ local wea = worldeditadditions
 return {
 	Perlin = dofile(wea.modpath.."/lib/noise/engines/perlin.lua"),
 	Sin = dofile(wea.modpath.."/lib/noise/engines/sin.lua")
+	
+	-- TODO: Follow https://www.redblobgames.com/articles/noise/introduction.html and implement different colours of noise (*especially* red and pink noise)
 }
