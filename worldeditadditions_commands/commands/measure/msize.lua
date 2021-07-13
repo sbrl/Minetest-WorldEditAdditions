f94906c85491efc3195afdec24053ca92003ddd6
VorTechnix <45538536+VorTechnix@users.noreply.github.com>
2021-07-13 09:08:56 -0700

    force output syntax in msize

--- worldeditadditions_commands/commands/measure/msize.lua
+++ worldeditadditions_commands/commands/measure/msize.lua
@@ -17,6 +17,6 @@ worldedit.register_command("msize", {
 		local vec = vector.subtract(worldedit.pos2[name],worldedit.pos1[name])
 		wea.vector.abs(vec)
 		
-		return true, str .. wea.table.tostring(vec)
+		return true, str .. "x: " .. vec.x .. ", y: " .. vec.y .. ", z: " .. vec.z
 	end,
 })
