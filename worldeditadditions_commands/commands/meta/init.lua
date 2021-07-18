d94ebd7e54940cf0e757dfdf2f2675a0c15eb98e
VorTechnix <45538536+VorTechnix@users.noreply.github.com>
2021-07-18 13:12:45 -0700

    separate init for meta commands

---
+++ worldeditadditions_commands/commands/meta/init.lua
@@ -0,0 +1,16 @@
+-- ███    ███ ███████ ████████  █████
+-- ████  ████ ██         ██    ██   ██
+-- ██ ████ ██ █████      ██    ███████
+-- ██  ██  ██ ██         ██    ██   ██
+-- ██      ██ ███████    ██    ██   ██
+
+-- Commands that work on other commands.
+
+local we_cm = worldeditadditions_commands.modpath .. "/commands/meta/"
+
+dofile(we_cm.."airapply.lua")
+dofile(we_cm.."elipsoidapply.lua")
+dofile(we_cm.."many.lua")
+dofile(we_cm.."multi.lua")
+dofile(we_cm.."subdivide.lua")
+-- dofile(we_cm.."for.lua")
