73e07eb01373cfb0311a29cc637701400ac55c58
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2021-07-19 00:26:52 +0100

    bit polyfill: tiny tweak

--- worldeditadditions/utils/bit.lua
+++ worldeditadditions/utils/bit.lua
@@ -18,14 +18,15 @@ if minetest.global_exists("bit") then
 	bit = bit
 else
 	bit = {}
+	
+	bit.bits = 32
+	bit.powtab = { 1 }
+	
+	for b = 1, bit.bits - 1 do
+		bit.powtab[#bit.powtab + 1] = math.pow(2, b)
+	end
 end
 
-bit.bits = 32
-bit.powtab = { 1 }
-
-for b = 1, bit.bits - 1 do
-	bit.powtab[#bit.powtab + 1] = math.pow(2, b)
-end
 
 -- Functions
 -- bit.band
