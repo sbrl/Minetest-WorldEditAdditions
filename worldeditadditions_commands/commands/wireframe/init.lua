d7d96ae2636d39f1c4d3c6892441b8ea93d69fef
VorTechnix <45538536+VorTechnix@users.noreply.github.com>
2021-07-20 12:52:19 -0700

    //wbox added

--- worldeditadditions_commands/commands/wireframe/init.lua
+++ worldeditadditions_commands/commands/wireframe/init.lua
@@ -8,6 +8,6 @@
 
 local we_cm = worldeditadditions_commands.modpath .. "/commands/wireframe/"
 
--- dofile(we_cm.."wbox.lua")
+dofile(we_cm.."wbox.lua")
 -- dofile(we_cm.."wcompass.lua")
 dofile(we_cm.."wcorner.lua")
---
+++ worldeditadditions_commands/commands/wireframe/wbox.lua
@@ -0,0 +1,36 @@
+-- ██     ██ ██████   ██████  ██   ██
+-- ██     ██ ██   ██ ██    ██  ██ ██
+-- ██  █  ██ ██████  ██    ██   ███
+-- ██ ███ ██ ██   ██ ██    ██  ██ ██
+--  ███ ███  ██████   ██████  ██   ██
+local wea = worldeditadditions
+local v3 = worldeditadditions.Vector3
+worldedit.register_command("wbox", {
+	params = "<node>",
+	description = "Set the corners of the current selection to <node>",
+	privs = {worldedit=true},
+	require_pos = 2,
+	parse = function(param)
+		local node = worldedit.normalize_nodename(param)
+		if not node then
+			return false, "invalid node name: " .. param
+		end
+		return true, node
+	end,
+	nodes_needed = function(name)
+		local delta = v3.subtract(worldedit.pos2[name], worldedit.pos1[name]):abs():add(1)
+		local total, mult, axes = 1, 4, {"x","y","z"}
+		for k,v in pairs(axes) do
+			if worldedit.pos1[name] ~= worldedit.pos2[name] then total = total*2
+			else mult = mult/2 end
+		end
+		for k,v in pairs(axes) do
+			if delta[v] > 2 then total = total + (delta[v] - 2)*mult end
+		end
+		return total
+	end,
+	func = function(name, node)
+		local _, count = wea.wire_box(worldedit.pos1[name], worldedit.pos2[name], node)
+		return _, count .. " nodes set"
+	end,
+})
