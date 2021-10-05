c8773efe22af7a6acb31712445c35a75c48b8b56
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2021-10-05 02:02:53 +0100

    Docs: Upgrade reference to support categorical display
    It even persists your preference using localStorage!

--- .docs/lib/parse_sections.js
+++ .docs/lib/parse_sections.js
@@ -1,7 +1,11 @@
+const crypto = require("crypto");
+
 const htmlentities = require("html-entities");
 const markdown = require("markdown-it")({
 	xhtmlOut: true
 });
+const chroma = require("chroma-js");
+
 
 const markdown_prism = require("markdown-it-prism");
 markdown.use(markdown_prism, {
@@ -16,9 +20,19 @@ markdown.use(markdown_prism, {
 	}
 });
 
-function make_section(acc) {
-	let title = acc[0].match(/#+\s+(.+)\s*/)[1].replace(/^`*|`*$/g, "");
+function extract_title(line) {
+	return line.match(/#+\s+(.+)\s*/)[1].replace(/^`*|`*$/g, "")
+}
+
+function make_section(acc, cat_current, cats) {
+	let title = extract_title(acc[0]);
 	return {
+		category: cat_current,
+		category_colour: cats.get(cat_current),
+		category_colour_dark: chroma(cats.get(cat_current))
+			.set("hsl.s", 0.8)
+			.set("hsl.l", "*0.6")
+			.css("hsl"),
 		title: htmlentities.encode(title),
 		slug: title.toLowerCase().replace(/[^a-z0-9-_\s]+/gi, "")
 			.replace(/\s+/g, "-")
@@ -28,13 +42,36 @@ function make_section(acc) {
 }
 
 module.exports = function parse_sections(source) {
+	const cats = new Map();
+	source.match(/^##\s+.*$/gm)
+		.map(extract_title)
+		.map((item, i, all) => cats.set(
+			item,
+			chroma(`hsl(${i/all.length*(360-360/all.length)}, 60%, 90%)`).css("hsl")
+		));
 	const lines = source.split(/\r?\n/gi);
 	const result = [];
 	let acc = [];
+	let cat_current = null;
 	for(let line of lines) {
-		if(line.startsWith(`#`) && !line.startsWith(`###`)) {
-			if(acc.length > 0)
-				result.push(make_section(acc));
+		
+		if(line.startsWith(`#`)) {
+			
+			// 1: Deal with the previous section
+			if(acc.length > 0) {
+				let heading_level_prev = acc[0].match(/^#+/)[0].length;
+				if(heading_level_prev === 3 && acc.length > 0) {
+					result.push(make_section(acc, cat_current, cats));
+				}
+				
+			}
+			
+			// 2: Deal with the new line
+			let heading_level = line.match(/^#+/)[0].length
+			
+			if(heading_level === 2)
+				cat_current = extract_title(line);
+			
 			acc = [ line ];
 		}
 		else
@@ -42,7 +79,7 @@ module.exports = function parse_sections(source) {
 	}
 	
 	if(acc.length > 0)
-		result.push(make_section(acc));
+		result.push(make_section(acc, cat_current, cats));
 	
-	return result;
+	return { sections: result, categories: cats };
 }
