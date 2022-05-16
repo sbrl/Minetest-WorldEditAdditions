10f350c967ae18736816ecd734b04ff67a59eeb6
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-05-16 21:21:15 +0100

    Delete copied code
    Given licence incompatibilities, we can't really copy code from 
    WorldEdit

--- worldeditadditions_commands/init.lua
+++ worldeditadditions_commands/init.lua
@@ -81,13 +81,3 @@ end
 
 
 dofile(we_c.modpath.."/aliases.lua")
-
-
---- Overrides to core WorldEdit commands
--- These are commented out for now, as they could be potentially dangerous to stability
--- Thorough testing is required of our replacement commands before these are uncommented
--- TODO: Depend on worldeditadditions_core before uncommenting this
--- BUG: //move+ seems to be leaving stuff behind for some strange reason --@sbrl 2021-12-26
--- worldeditadditions_core.alias_override("copy", "copy+")
--- worldeditadditions_core.alias_override("move", "move+") -- MAY have issues where it doesn't overwrite the old region properly, but haven't been able to reliably reproduce this
--- worldeditadditions_core.alias_override("replace", "replacemix")
