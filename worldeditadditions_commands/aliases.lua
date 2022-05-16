9d3a4ce263885e08d0f9edf8a0fc12d4d3d10ee2
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-05-16 20:33:04 +0100

    refactor aliases into a separate file

---
+++ worldeditadditions_commands/aliases.lua
@@ -0,0 +1,11 @@
+worldedit.alias_command("smoothadv", "convolve")
+worldedit.alias_command("conv", "convolve")
+
+worldedit.alias_command("naturalise", "layers")
+worldedit.alias_command("naturalize", "layers")
+
+worldedit.alias_command("flora", "bonemeal")
+
+-- Measure Tools
+worldedit.alias_command("mcount", "count")
+worldedit.alias_command("mfacing", "mface")
--- worldeditadditions_commands/init.lua
+++ worldeditadditions_commands/init.lua
@@ -80,17 +80,7 @@ end
 -- end
 
 
-worldedit.alias_command("smoothadv", "convolve")
-worldedit.alias_command("conv", "convolve")
-
-worldedit.alias_command("naturalise", "layers")
-worldedit.alias_command("naturalize", "layers")
-
-worldedit.alias_command("flora", "bonemeal")
-
--- Measure Tools
-worldedit.alias_command("mcount", "count")
-worldedit.alias_command("mfacing", "mface")
+dofile(we_c.modpath.."/aliases.lua")
 
 
 --- Overrides to core WorldEdit commands
