be8e097f1f5324e658a5b34311ec378453bfed22
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2023-08-15 17:44:19 +0100

    tests: grammar

--- .tests/strings/trim.test.lua
+++ .tests/strings/trim.test.lua
@@ -1,43 +1,43 @@
 local polyfill = require("worldeditadditions_core.utils.strings.polyfill")
 
 describe("trim", function()
-	it("work for a string that's already trimmed", function()
+	it("should work for a string that's already trimmed", function()
 		assert.are.equal(
 			polyfill.trim("test"),
 			"test"
 		)
 	end)
-	it("trim from the start", function()
+	it("should trim from the start", function()
 		assert.are.equal(
 			polyfill.trim("     test"),
 			"test"
 		)
 	end)
-	it("trim from the end", function()
+	it("should trim from the end", function()
 		assert.are.equal(
 			polyfill.trim("test     "),
 			"test"
 		)
 	end)
-	it("trim from both ends", function()
+	it("should trim from both ends", function()
 		assert.are.equal(
 			polyfill.trim("   test     "),
 			"test"
 		)
 	end)
-	it("trim another string", function()
+	it("should trim another string", function()
 		assert.are.equal(
 			polyfill.trim("yay    "),
 			"yay"
 		)
 	end)
-	it("trim tabs", function()
+	it("should trim tabs", function()
 		assert.are.equal(
 			polyfill.trim("//forest		"),
 			"//forest"
 		)
 	end)
-	it("avoid trimming spaces in the middle", function()
+	it("should avoid trimming spaces in the middle", function()
 		assert.are.equal(
 			polyfill.trim("te   st     "),
 			"te   st"
