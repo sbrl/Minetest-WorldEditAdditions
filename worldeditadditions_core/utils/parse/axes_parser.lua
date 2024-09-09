7e9e0f6a48b945f2de05b138b3470d9c2582c809
VorTechnix <45538536+VorTechnix@users.noreply.github.com>
2024-09-09 12:19:54 -0700

    Finally committing this

--- worldeditadditions_core/utils/parse/axes_parser.lua
+++ worldeditadditions_core/utils/parse/axes_parser.lua
@@ -16,13 +16,10 @@ end
 
 local wea_c = worldeditadditions_core or nil
 local Vector3
-local key_instance
 if worldeditadditions_core then
-	key_instance = dofile(wea_c.modpath.."/utils/parse/key_instance.lua")
 	Vector3 = dofile(wea_c.modpath.."/utils/vector3.lua")
 else
 	Vector3 = require("worldeditadditions_core.utils.vector3")
-	key_instance = require("worldeditadditions_core.utils.parse.key_instance")
 end
 
 
@@ -30,34 +27,34 @@ end
 local keywords = {
 	-- Compass keywords
 	compass = {
-		n = "z", north = "z",
+		["n"] = "z", ["north"] = "z",
 		["-n"] = "-z", ["-north"] = "-z",
-		s = "-z", south = "-z",
+		["s"] = "-z", ["south"] = "-z",
 		["-s"] = "z", ["-south"] = "z",
-		e = "x", east = "x",
+		["e"] = "x", ["east"] = "x",
 		["-e"] = "-x", ["-east"] = "-x",
-		w = "-x", west = "-x",
+		["w"] = "-x", ["west"] = "-x",
 		["-w"] = "x", ["-west"] = "x",
+		["u"] = "y", ["up"] = "y",
+		["d"] = "-y", ["down"] = "-y",
 	},
 	
 	-- Direction keywords
 	dir = {
-		["?"] = "front", f = "front",
-		facing = "front", front = "front",
-		b = "back", back = "back",
-		behind = "back", rear = "back",
-		l = "left", left = "left",
-		r = "right", right = "right",
-		u = "up", up = "up",
-		d = "down", down = "down",
+		["?"] = "front", ["f"] = "front",
+		["facing"] = "front", ["front"] = "front",
+		["b"] = "back", ["back"] = "back",
+		["behind"] = "back", ["rear"] = "back",
+		["l"] = "left", ["left"] = "left",
+		["r"] = "right", ["right"] = "right",
 	},
 	
 	-- Mirroring keywords
 	mirroring = {
-		sym = true, symmetrical = true,
-		mirror = true, mir = true,
-		rev = true, reverse = true,
-		["true"] = true
+		["sym"] = true, ["symmetrical"] = true,
+		["mirror"] = true, ["mir"] = true,
+		["rev"] = true, ["reverse"] = true,
+		["m"] = true, ["true"] = true
 	},
 }
 
@@ -69,17 +66,23 @@ local parse = {}
 -- @param: str: String: Axis declaration to parse
 -- @returns: Table|Bool: axis | axes | false
 function parse.axis(str)
-	local axes, ret = {"x","y","z"}, {}
-	for i,v in ipairs(axes) do
+	local ret = {}
+	if str:match("[^hvxyz]") then return false end
+	
+	if str:match("v") then 
+		ret["v"] = true
+		str = "y"..str
+	end
+	if str:match("h") then 
+		ret["h"] = true
+		str = "xz"..str
+	end
+
+	for _,v in ipairs({"x", "y", "z"}) do
 		if str:match(v) then table.insert(ret,v) end
 	end
-	if #ret > 0 and str:match("^[xyz]+$") then
-		return ret
-	elseif str == "h" then
-		return {"x", "z"}
-	elseif  str == "v" then
-		return {"y"}
-	else return false end
+
+	return ret
 end
 
 --- Processes an number from a string.
@@ -98,7 +101,7 @@ end
 -- @returns: Key Instance: returns keyword type, processed keyword content and signed number (or nil)
 function parse.keyword(str)
 	if type(str) ~= "string" then
-		return key_instance.new("err", "Error: \""..tostring(str).."\" is not a string.", 404)
+		return "err", "Error: \""..tostring(str).."\" is not a string.", 404
 	elseif keywords.compass[str] then
 		str = keywords.compass[str]
 	end
@@ -110,14 +113,16 @@ function parse.keyword(str)
 	
 	local axes = parse.axis(str)
 	if axes then
-		return key_instance.new("axis", axes, sign)
+		return "axis", axes, sign
 	elseif keywords.dir[str] then
-		return key_instance.new("dir", keywords.dir[str], sign)
+		return "dir", keywords.dir[str], sign
 	elseif keywords.mirroring[str] then
-		return key_instance.new("rev", "mirroring")
-	else return key_instance.new("err", "Error: \""..str.."\" is not a valid axis, direction or keyword.", 422)
+		return "rev", "mirroring"
+	else
+		return "err", "Error: \""..str.."\" is not a valid axis, direction or keyword.", 422
 	end
 end
+
 
 --- Creates a vector with a length of (@param: value * @param: sign)
 --  on each axis in @param: axes.
@@ -126,12 +131,15 @@ end
 -- @param: sign: Number: sign multiplier for axes
 -- @returns: Vector3: processed vector
 function parse.vectorize(axes,value,sign)
+	-- TODO: Add hv compatability
 	local ret = Vector3.new()
-	for i,v in ipairs(axes) do
+	for _,v in ipairs(axes) do
 		ret[v] = value * sign
 	end
 	return ret
 end
+
+
 
 --- Converts Unified Axis Keyword table into Vector3 instances.
 -- @param: tbl: Table: Keyword table to parse
@@ -141,78 +149,74 @@ end
 -- if error: @returns: false, String: error message
 function parse.keytable(tbl, facing, sum)
 	local min, max = Vector3.new(), Vector3.new()
-	local expected, tmp = 1, {axes = {}, num = 0, sign = 1, mirror = false}
-	function tmp:reset() self.axis, self.sign = "", 1 end
+	local expected = 1
+	local tmp = {axes = {}, num = 0, sign = 1, mirror = false}
 	
-	for i,v in ipairs(tbl) do
-		if v:sub(1,1) == "+" then v = v:sub(2) end
+	function tmp:reset() 
+		self.axis, self.sign = "", 1 
+	end
+	
+	--- Processes a number and adds it to the min and max vectors.
+	-- @param num The number to process.
+	-- @param axes The axes to apply the number to.
+	-- @param sign The sign of the number.
+	local function parseNumber(num, axes, sign)
+		if num * sign >= 0 then
+			max = max:add(parse.vectorize(axes, num, sign))
+		else
+			min = min:add(parse.vectorize(axes, num, sign))
+		end
+	end
+	
+	for i, v in ipairs(tbl) do
+		if v:sub(1, 1) == "+" then 
+			v = v:sub(2) 
+		end
+		
 		tmp.num = parse.num(v)
-		if expected == 1 then -- Mode 1 of state machine
-			-- State machine expects string
+		
+		if expected == 1 then
 			if tmp.num then
-				-- If this is a number treat as all axes and add to appropriate vector
-				if tmp.num * tmp.sign >= 0 then
-					max = max:add(parse.vectorize({"x","y","z"}, tmp.num, tmp.sign))
-				else
-					min = min:add(parse.vectorize({"x","y","z"}, tmp.num, tmp.sign))
-				end
-				-- We are still looking for axes so the state machine should remain
-				-- in Mode 1 for the next iteration
+				parseNumber(tmp.num, {"x", "y", "z", h = true, v = true}, tmp.sign)
 			else
-				-- Else parse.keyword
-				local key_inst = parse.keyword(v)
-				-- Stop if error and return message
-				if key_inst:is_error() then return false, key_inst.entry end
-				-- Check key type and process further
-				if key_inst.type == "axis" then
-					tmp.axes = key_inst.entry
-					tmp.sign = key_inst.sign
-				elseif key_inst.type == "dir" then
-					tmp.axes = {facing[key_inst.entry].axis}
-					tmp.sign = facing[key_inst.entry].sign * key_inst.sign
-				elseif key_inst.type == "rev" then
+				local key_type, key_entry, key_sign = parse.keyword(v)
+				
+				if key_type == "axis" then
+					tmp.axes = key_entry
+					tmp.sign = key_sign
+				elseif key_type == "dir" then
+					tmp.axes = {facing[key_entry].axis}
+					tmp.sign = facing[key_entry].sign * key_sign
+				elseif key_type == "rev" then
 					tmp.mirror = true
 				else
-					-- If key type is error or unknown throw error and stop
-					if key_inst.type == "err" then
-						return false, key_inst.entry
-					else
-						return false, "Error: Unknown Key Instance type \""..
-						tostring(key_inst.type).."\". Contact the devs!"
-					end
+					return false, key_entry
 				end
-				expected = 2 -- Toggle state machine to expect number (Mode 2)
+				
+				expected = 2
 			end
-			
-		else -- Mode 2 of state machine
-			-- State machine expects number
+		else
 			if tmp.num then
-				-- If this is a number process num and add to appropriate vector
-				if tmp.num * tmp.sign >= 0 then
-					max = max:add(parse.vectorize(tmp.axes, tmp.num, tmp.sign))
-				else
-					min = min:add(parse.vectorize(tmp.axes, tmp.num, tmp.sign))
-				end
-				expected = 1 -- Toggle state machine to expect string (Mode 1)
+				parseNumber(tmp.num, tmp.axes, tmp.sign)
+				expected = 1
 			else
-				-- Else throw an error and stop everything
-				return false, "Error: Expected number after \""..tostring(tbl[i-1])..
-					"\", got \""..tostring(v).."\"."
+				return false, "Error: Expected number after \""..tostring(tbl[i-1]).."\". Got \""..tostring(v).."\"."
 			end
-		end -- End of state machine
-		
-	end -- End of main for loop
+		end
+	end
 	
-	-- Handle Mirroring
 	if tmp.mirror and not sum then
 		max = max:max(min:abs())
 		min = max:multiply(-1)
 	end
 	
-	if sum then return min:add(max)
-	else return min, max end
-	
+	if sum then 
+		return min:add(max)
+	else 
+		return min, max 
+	end
 end
+
 
 return {
 	keyword = parse.keyword,
