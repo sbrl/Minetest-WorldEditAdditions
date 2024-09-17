d49f6d2131770fff94c08e0e6a589dbc38374fa9
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2024-09-17 23:18:37 +0100

    Lift orientation functions from screwdriver2 mod
    Ref @12Me21 https://github.com/12Me21/screwdriver2/blob/master/init.lua#L75-L79
    Ref @khonkhortisan https://forum.minetest.net/viewtopic.php?p=73195&sid=1d2d2e4e76ce2ef9c84646481a4b84bc#p73195
    
    ....if either of you have an issue with this, please get in touch! The licence seems to allow this lifting and relicencing like this (given it's WTFPL as of the time of typing), but correct me if I'm wrong.
    
    For reference, WorldEditAdditions is currently licenced under the Mozilla Public License Version 2.0.

--- worldeditadditions_core/init.lua
+++ worldeditadditions_core/init.lua
@@ -30,6 +30,7 @@ wea_c.Set = dofile(wea_c.modpath.."/utils/set.lua")
 wea_c.Vector3 = dofile(wea_c.modpath.."/utils/vector3.lua")
 wea_c.Mesh, wea_c.Face = dofile(wea_c.modpath.."/utils/mesh.lua")
 wea_c.rotation = dofile(wea_c.modpath .. "/utils/rotation.lua")
+wea_c.orientation = dofile(wea_c.modpath .. "/utils/orientation.lua")
 wea_c.param2 = dofile(wea_c.modpath .. "/utils/param2.lua")
 
 wea_c.Queue = dofile(wea_c.modpath.."/utils/queue.lua")
