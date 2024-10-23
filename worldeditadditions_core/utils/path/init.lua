9f85a7ca0d3968513a45dc10a9c970b29a4aed04
VorTechnix <45538536+VorTechnix@users.noreply.github.com>
2024-10-23 16:12:19 -0700

    Update init.lua

--- worldeditadditions_core/utils/path/init.lua
+++ worldeditadditions_core/utils/path/init.lua
@@ -1,5 +1,5 @@
 local wea_c = worldeditadditions_core
 
 wea_c.path = {
-	file_path = dofile(wea_c.modpath.."/utils/format/file_path.lua"),
+	join = dofile(wea_c.modpath.."/utils/format/join.lua"),
 }
