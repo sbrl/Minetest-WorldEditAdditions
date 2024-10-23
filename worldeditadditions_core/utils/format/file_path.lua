8f54522e2020cbefba74f088ab0a15a16358e72d
VorTechnix <45538536+VorTechnix@users.noreply.github.com>
2024-10-23 15:18:35 -0700

    Create file_path.lua

---
+++ worldeditadditions_core/utils/format/file_path.lua
@@ -0,0 +1,17 @@
+--- 
+-- @module worldeditadditions_core
+local wea_c = worldeditadditions_core
+
+--- Joins the given path segments into a single path with dirsep.
+--	@param	...	string	The path fragments to process and join.
+--	@return		string	The joined path.
+--	@example	Basic usage
+--		local path = file_path("C:\\Users", "me", "/Documents/code.lua")
+local file_path = function( ... )
+	local path = { ... }
+	for i, v in ipairs(path) do path[i] = tostring(v) end
+	return ({table.concat(path, wea_c.dirsep)
+			:gsub("[/\\]+", wea_c.dirsep)})[1]
+end
+
+return file_path
